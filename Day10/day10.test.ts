763d0c36480fb9f366c74e251751a78b90c1296d
Albin Ljunghusen <albin.ljunghusen@isotop.se>
2021-12-11 08:30:41 +0100

    wip2

--- Day10/day10.test.ts
+++ Day10/day10.test.ts
@@ -15,54 +15,38 @@ const part1 = pipe(
 );
 
 type charmap = {
-  a: number;
-  b: number;
-  c: number;
-  d: number;
+  "(": number;
+  ")": number;
+  "[": number;
+  "]": number;
+  "{": number;
+  "}": number;
+  "<": number;
+  ">": number;
 };
 
 type stuff = "{" | "}" | "(" | ")" | "[" | "]" | "<" | ">";
 
+const asciTen = { 4: ")", 6: "]", 9: "}", 12: ">" };
+
 const findCorruptingCharacter = (
-  chs: string[],
-  map: charmap = { a: 0, b: 0, c: 0, d: 0 }
+  str: string,
+  map: {
+    4: 0;
+    6: 0;
+    9: 0;
+    12: 0;
+  }
 ) => {
-  console.log(head(chs));
-  console.log(chs.length);
-  return match(head(chs) as stuff)
-    .with("(", () =>
-      findCorruptingCharacter(tail(chs), { ...map, a: map.a + 1 })
-    )
-    .with("[", () =>
-      findCorruptingCharacter(tail(chs), { ...map, b: map.b + 1 })
-    )
-    .with("{", () =>
-      findCorruptingCharacter(tail(chs), { ...map, c: map.c + 1 })
-    )
-    .with("<", () =>
-      findCorruptingCharacter(tail(chs), { ...map, d: map.d + 1 })
-    )
-    .with(")", (e) =>
-      a === 0 ? e : findCorruptingCharacter(tail(chs), { a: a - 1, b, c, d })
-    )
-    .with("]", (e) =>
-      b === 0 ? e : findCorruptingCharacter(tail(chs), { a, b: b - 1, c, d })
-    )
-    .with("}", (e) =>
-      c === 0 ? e : findCorruptingCharacter(tail(chs), { a, b, c: c - 1, d })
-    )
-    .with(">", (e) =>
-      d === 0 ? e : findCorruptingCharacter(tail(chs), { a, b, c, d: d - 1 })
-    )
-    .exhaustive();
+  const tenner = Math.floor(str[0] / 10);
+
+  return findCorruptingCharacter(str.slice(1));
 };
 
 const characterToScore = (line: string) => {};
 
 test("part1", () => {
-  expect(
-    findCorruptingCharacter(Array.from("{([(<{}[<>[]}>{[]{[(<()>"))
-  ).toEqual("}");
+  expect(findCorruptingCharacter("{([(<{}[<>[]}>{[]{[(<()>")).toEqual("}");
   // expect(part1(example)).toEqual(15);
   // expect(part1(input)).toEqual(448);
 });
