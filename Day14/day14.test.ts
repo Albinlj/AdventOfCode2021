3b1b61ad5b6e487685cee03049f86b585ff069fc
Albin Ljunghusen <albin.ljunghusen@isotop.se>
2021-12-15 09:10:48 +0100

    Day 14

--- Day14/day14.test.ts
+++ Day14/day14.test.ts
@@ -1,22 +1,13 @@
 import { getInput, getExample } from "../utils/getInput";
-import {
-  add,
-  aperture,
-  concat,
-  countBy,
-  head,
-  last,
-  mergeWith,
-  splitEvery,
-  sum,
-} from "ramda";
-import { string } from "fp-ts";
+import { aperture, countBy, head, last, memoizeWith, mergeWith } from "ramda";
 
 const input = getInput(__dirname);
 const example = getExample(__dirname);
 
 type Rules = Map<string, string>;
+type LetterCount = { [letter: string]: number };
 
+//PARSING
 const parse = (input: string): [string, Rules] => {
   const [polymer, rules] = input.split("\n\n");
   const parsedRules = rules
@@ -25,71 +16,60 @@ const parse = (input: string): [string, Rules] => {
   return [polymer, new Map(parsedRules) as Rules];
 };
 
+//PART1
 const part1 = (input: string, times: number = 10) => {
   const [polymer, rules] = parse(input);
   const final = applyRules(polymer, rules, times);
-
-  const counts = countBy((s) => s as any, final as any);
-  let ordered = Object.values(counts);
-  ordered.sort((a, b) => b - a);
-
-  return head(ordered) - last(ordered);
+  const counts = countBy<string>((s) => s, final as any])
+  return differenceCommonUncommon(counts);
 };
 
+const applyRules = (polymer: string, rules: Rules, times: number): string => {
+  const pairs = aperture(2, Array.from(polymer));
+  const merged =
+    polymer[0] + pairs.map(([a, b]) => rules.get(a + b) + b).join("");
+  return times === 1 ? merged : applyRules(merged, rules, times - 1);
+};
+
+//PART2
 const part2 = (input: string, times: number = 10) => {
   const [polymer, rules] = parse(input);
-  const final = applyRulesGetCounts(polymer, rules, times);
+  const applyRulesGetCounts = memoizeWith(
+    ([a, c]) => a + c,
+    ([[a, c], times]: [string, number]): LetterCount => {
+      const b = rules.get(a + c);
 
-  let ordered = Object.values(final);
+      return times === 0
+        ? countBy<string>((s) => s, [a])
+        : mergeObj(
+            applyRulesGetCounts([a + b, times - 1]),
+            applyRulesGetCounts([b + c, times - 1])
+          );
+    }
+  );
+
+  const counts = aperture(2, Array.from(polymer))
+    .map((arr: string[]) => applyRulesGetCounts([arr.join(""), times]))
+    .reduce(mergeObj, {});
+
+  counts[last(polymer)] += 1;
+  return differenceCommonUncommon(counts);
+};
+
+const differenceCommonUncommon = (counts: LetterCount) => {
+  let ordered = Object.values(counts);
   ordered.sort((a, b) => b - a);
   return head(ordered) - last(ordered);
 };
 
-const applyRules = (
-  polymer: string,
-  rules: Rules,
-  times: number = 1
-): string => {
-  const pairs = aperture(2, Array.from(polymer));
-  const ne = polymer[0] + pairs.map(([a, b]) => rules.get(a + b) + b).join("");
-  return times === 1 ? ne : applyRules(ne, rules, times - 1);
-};
+const mergeObj = (a: LetterCount, b: LetterCount): LetterCount =>
+  mergeWith((a, b) => a + b, a, b);
 
-const applyRulesGetCounts = (
-  polymer: string,
-  rules: Rules,
-  times: number = 1,
-  count: number = 10000
-): { [index: string]: number } => {
-  const pairs = aperture(2, Array.from(polymer));
-  const merge =
-    polymer[0] + pairs.map(([a, b]) => rules.get(a + b) + b).join("");
-  return times === 1
-    ? letterCount(merge)
-    : splitEvery(count, merge)
-        .map((part, i, arr) => part + arr[i][count - 1])
-        .reduce(
-          (acc, curr) =>
-            mergeWith(
-              (a, b) => a + b,
-              acc,
-              applyRulesGetCounts(curr, rules, times - 1)
-            ),
-          {}
-        );
-};
-
-const sumObjectStuffs = (a: object, b: object): object => mergeWith(add, a, b);
-
-const letterCount = (polymer: string) => {
-  return countBy<string>((s) => s, polymer as any);
-};
-
+//TESTS
 test("part2", () => {
   expect(part2(example, 10)).toEqual(1588);
-  expect(part2(input, 10)).toEqual(4517);
-  // expect(part2(input, 20)).toEqual(4495149);
-  // expect(part2(input, 40)).toEqual(4517);
+  expect(part2(example, 40)).toEqual(2188189693529);
+  expect(part2(input, 40)).toEqual(4704817645083);
 });
 
 test("part1", () => {
